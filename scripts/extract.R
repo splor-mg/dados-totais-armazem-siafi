d8afb7e8054c051700b1d33a62b5cc4a125a674d
hslinhares <henrique.s.linhares@gmail.com>
2023-10-18 12:02:43 -0300

    permite salvar anexo com outro nome

--- scripts/extract.R
+++ scripts/extract.R
@@ -6,21 +6,42 @@ gm_auth(token = gm_token_read(
 ))
 
 
-
 #retorna message_id e thread_id
-msg = gm_messages(
-  search =  "subject:(totais siafi) .csv  newer_than:1d"
+
+msg_hist = gm_messages(
+  search =  "subject:(historico) .csv  newer_than:1d"
 )
 
 #retorna message_id
-ids = gm_id(msg)
+ids_hist = gm_id(msg_hist)
 
-#salva o anexo
-for (i in ids){
-  gm_save_attachments(gm_message(i), path = 'data-raw')
+#retorna a message, lista que contém informações entre elas o attachmentId
+message = gm_message(ids_hist)
+
+
+#extrai attachmentId independemente da sua posição na lista recursiva
+extract_data_by_name <- function(list, name) {
+  result <- list()
+  
+  if (length(ids_hist == 1)){
+        recursive_extract <- function(x) {
+    if (is.list(x)) {
+      if (name %in% names(x)) {
+        result <<- c(result, list(x[[name]]))
+      } else {
+        lapply(x, recursive_extract)
+      }
+    }
+    }
+  recursive_extract(list)
+  return(result[[1]])
+}
 }
 
-
+attachment_id <- extract_data_by_name(message, 'attachmentId')
+#salva o anexo renomeando-o
+my_attachment = gm_attachment(attachment_id, ids_hist)
+gm_save_attachment(my_attachment, "data-raw/totais_siafi_historico.csv")
 
   
 

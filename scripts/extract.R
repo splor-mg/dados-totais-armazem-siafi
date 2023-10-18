f3f3ebe95198bb68d13e80f7d8c7519334c3ec0e
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-10-18 12:44:48 -0300

    utiliza gm_attachments para extrair attachments

--- scripts/extract.R
+++ scripts/extract.R
@@ -18,27 +18,7 @@ ids_hist = gm_id(msg_hist)
 #retorna a message, lista que contém informações entre elas o attachmentId
 message = gm_message(ids_hist)
 
-
-#extrai attachmentId independemente da sua posição na lista recursiva
-extract_data_by_name <- function(list, name) {
-  result <- list()
-  
-  if (length(ids_hist == 1)){
-        recursive_extract <- function(x) {
-    if (is.list(x)) {
-      if (name %in% names(x)) {
-        result <<- c(result, list(x[[name]]))
-      } else {
-        lapply(x, recursive_extract)
-      }
-    }
-    }
-  recursive_extract(list)
-  return(result[[1]])
-}
-}
-
-attachment_id <- extract_data_by_name(message, 'attachmentId')
+attachment_id <- gm_attachments(message)[["id"]]
 #salva o anexo renomeando-o
 my_attachment = gm_attachment(attachment_id, ids_hist)
 gm_save_attachment(my_attachment, "data-raw/totais_siafi_historico.csv")

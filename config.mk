19dc414e29c90f600b92287dfc684e731f14916b
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-03-01 18:18:23 -0300

    /usr/bin/head: error reading 'standard input': Broken pipe

--- config.mk
+++ config.mk
@@ -2,10 +2,7 @@ ifeq ($(shell uname), Darwin)
     PYTHON = python3
 else ifeq ($(shell uname), Linux)
     PYTHON = python3
-else ifeq ($(shell uname | head -c 5) , MINGW)
-# git bash needs prefixing with winpty
-    PYTHON = python
 else
-# for cmd and powershell
+# for windows
     PYTHON = python
 endif

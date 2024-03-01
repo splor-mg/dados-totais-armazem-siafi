cf8281132d02af01f8392fc4908a2440d4ab8d2c
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-03-01 17:55:40 -0300

    utiliza python no lugar de py no windows

--- config.mk
+++ config.mk
@@ -4,8 +4,8 @@ else ifeq ($(shell uname), Linux)
     PYTHON = python3
 else ifeq ($(shell uname | head -c 5) , MINGW)
 # git bash needs prefixing with winpty
-    PYTHON = py -3
+    PYTHON = python
 else
 # for cmd and powershell
-    PYTHON = py -3
+    PYTHON = python
 endif

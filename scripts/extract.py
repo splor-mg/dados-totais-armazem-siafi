2cc742d9bee72bcef5543474dfc5d4e82d73407e
hslinhares <henrique.s.linhares@gmail.com>
2024-04-03 15:00:41 -0300

    Corrige o erro no such file or directory subprocess
    Closes https://github.com/splor-mg/armazem-siafi-totais-dados/issues/28

--- scripts/extract.py
+++ scripts/extract.py
@@ -4,4 +4,4 @@ import subprocess
 logger = logging.getLogger(__name__)
 
 def extract_resource(resource_name: str = None, descriptor: str = 'datapackage.yaml'):
-    subprocess.run('Rscript scripts/extract.R')
+    subprocess.call('Rscript scripts/extract.R', shell=True)

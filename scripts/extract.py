8a0d751be1ff0315defd99294693802c6db475df
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-04-04 12:24:48 -0300

    check exit code when calling Rscript from python
    Closes https://github.com/splor-mg/armazem-siafi-dados-2024/issues/39

--- scripts/extract.py
+++ scripts/extract.py
@@ -4,4 +4,4 @@ import subprocess
 logger = logging.getLogger(__name__)
 
 def extract_resource(resource_name: str = None, descriptor: str = 'datapackage.yaml'):
-    subprocess.call('Rscript scripts/extract.R', shell=True)
+    subprocess.run(['Rscript', 'scripts/extract.R'], check=True)

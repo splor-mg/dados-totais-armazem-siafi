4cd8181149d9561c5484f9c63b891c8325b3830f
Henrique de Souza Linhares <43452663+hslinhares@users.noreply.github.com>
2024-11-18 10:33:38 -0300

    Adiciona informações de log sobre as bases que não chegam no gmail (#49)
    * adiciona função get_attachment_name
    
    * adiciona attachment info
    
    * substitui hífen por underline em gmail files

--- scripts/extract.R
+++ scripts/extract.R
@@ -5,7 +5,12 @@ library(glue)
 source("scripts/utils.R")
 
 package <- read_package("datapackage.yaml")
-data_files_count <- map(package$resources, "path") |> unlist() |> length()
+datapackage_files <- map(package$resources, "path") |> 
+  unlist() |>
+  stringr::str_remove("^data-raw/")
+  
+datapackage_files_count <- datapackage_files |> 
+  length() 
 
 gmailr::gm_auth(token = gmailr::gm_token_read(
   path = "gmail.rds",
@@ -21,8 +26,20 @@ attachment_ids <- gmailr::gm_messages(search_query) |>
 
 attachments_count <- length(attachment_ids)
 
-if (data_files_count != attachments_count) {
-  stop(glue("Expected {data_files_count} data files but only found {attachments_count} in gmail"))
-}
 
+if (datapackage_files_count != attachments_count) {
+  
+  gmail_files <- purrr::map(attachment_ids, get_attachment_name) |> map(~ gsub("-", "_", .))
+  extra_files <- setdiff(unlist(gmail_files), datapackage_files)
+  missing_files <- setdiff(datapackage_files, unlist(gmail_files))
+
+  if(length(extra_files) > 0){
+    logger::log_warn("O filtro de busca do gmail trouxe o arquivo adicional '{extra_files}'")
+  }
+
+  if(length(missing_files) > 0){
+    logger::log_warn("O filtro de busca do gmail não trouxe o arquivo '{missing_files}'")
+  }
+  stop(glue("Expected {datapackage_files_count} data files but found {attachments_count} in gmail"))
+}
 purrr::walk(attachment_ids,write_attachment)

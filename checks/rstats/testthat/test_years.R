980c445d6fc67dd0d072e9c477da96f6b3aba33c
hslinhares <henrique.s.linhares@gmail.com>
2025-02-05 12:24:37 -0300

    aumenta coberta temporal para até 2025

--- checks/rstats/testthat/test_years.R
+++ checks/rstats/testthat/test_years.R
@@ -1,5 +1,5 @@
 test_that("cobertura temporal anos totais_siafi_execucao_cota", {  
-  expected <- 2002:year(Sys.Date())
+  expected <- 2002:2025
   result <- sort(unique(package$totais_siafi_execucao_cota$ano))
   
   expect_equal(result, expected)
@@ -7,14 +7,14 @@ test_that("cobertura temporal anos totais_siafi_execucao_cota", {
 
 
 test_that("cobertura temporal anos totais_siafi_execucao", {  
-  expected <- 2002:year(Sys.Date())
+  expected <- 2002:2025
   result <- sort(unique(package$totais_siafi_execucao$ano))
   
   expect_equal(result, expected)
 })
 
 test_that("cobertura temporal anos totais_siafi_restos_pagar", {  
-  expected <- 2002:year(Sys.Date())
+  expected <- 2002:2025
   result <- sort(unique(package$totais_siafi_restos_pagar$ano))
   
   expect_equal(result, expected)

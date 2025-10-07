bbdcf19b1ad84e108b9ce8d7dfb1a738aa274542
hslinhares <henrique.s.linhares@gmail.com>
2025-10-07 18:40:05 -0300

    ajusta script delete email, fix #89, fix #90, fix #91

--- scripts/delete-email.R
+++ scripts/delete-email.R
@@ -5,9 +5,9 @@ gmailr::gm_auth(token = gmailr::gm_token_read(
 ))
 
 
-search_query_old <- "subject:(totais) filename:.csv filename:.csv older_than:15d"
+search_query_old <- "older_than:15d"
 
-gmailr::gm_messages(search_query_old)|>
+gmailr::gm_messages(search_query_old, num_results = 1000)|>
 gmailr::gm_id() |>
 purrr::walk(gmailr::gm_delete_message)
 
